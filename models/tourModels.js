cbb41ebe8feb336cf2f9ad1248a28a59e3ee82f1
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-19 19:46:37 +0300

    Commit

--- models/tourModels.js
+++ models/tourModels.js
@@ -4,15 +4,61 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: [true, 'A tour must have a name']
+    required: [true, 'A tour must have a name'],
+    unique: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
   }
 });
 
