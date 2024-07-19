bbc77dae608b3341a72f711d0d07df774a9879d9
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-19 15:01:53 +0300

    Commit

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch(err => {
+    console.error('DB connection error:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
